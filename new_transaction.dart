5a4ba8776aa66cd131d8c2f09f7cab6b1e4ffcf6
om888 <58175399+om888@users.noreply.github.com>
2019-11-25 18:46:08 +0530

    Add files via upload

---
+++ new_transaction.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class NewTransation extends StatefulWidget {
+  final Function addtxc;
+
+  NewTransation(this.addtxc);
+  @override
+  _NewTransactionState createState() => _NewTransactionState();
+}
+
+class _NewTransactionState extends State<NewTransation> {
+  final titleController= TextEditingController();
+  final amountController=TextEditingController();
+
+
+  void submitData()
+{
+  final enteredTitle = titleController.text;
+  final enteredAmount=double.parse(amountController.text);
+  if(enteredTitle.isEmpty||enteredAmount<=0)
+    {
+      return;
+    }
+
+  widget.addtxc(
+
+    enteredTitle,
+      enteredAmount,
+        );
+  Navigator.of(context).pop();
+}
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(
+                  labelText: 'Title'),
+              controller: titleController,
+              onSubmitted: (_) => submitData(),
+              //onChanged: (val){
+              //    titleInput = val;
+              // },
+            ),
+            TextField(
+              decoration: InputDecoration(
+                  labelText: 'Amount'),
+              controller: amountController,
+              keyboardType: TextInputType.number,
+              onSubmitted: (_) => submitData(),
+              // onChanged: (val) => amountInput=val,
+            ),
+            FlatButton(
+              child: Text('Add Transaction'),
+              textColor: Colors.purple,
+              onPressed: submitData,
+    ),
+],
+        ),
+            ),
+    );
+
+
+  }
+}
---
+++ transaction_list.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import '../models/transaction.dart';
+import 'package:intl/intl.dart';
+
+class TransactionList extends StatelessWidget {
+final List<Transaction> transactions;
+TransactionList(this.transactions);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+        child: ListView(
+            children: transactions.map((txc) {
+              return Card(
+                child: Row(children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.symmetric(
+                        vertical: 10, horizontal: 15),
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                                color: Theme.of(context).primaryColor,
+                                width: 2),
+                          ),
+                      padding: EdgeInsets.all(10),
+                    child: Text
+                      ('\$${txc.amount.toStringAsFixed(2)}',
+                         style: TextStyle(
+                           fontWeight: FontWeight.bold,
+                           fontSize: 20,
+                           color: Theme.of(context).primaryColor,
+                         ),
+                    ),
+                  ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(txc.title,
+                  style: TextStyle(fontSize: 18,
+                      fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  DateFormat.yMMMd().format(txc.date),
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            )
+                ],
+                ),
+              );
+            }).toList(),
+          ),
+
+    );
+  }
+}
